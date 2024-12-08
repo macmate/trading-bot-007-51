e098bb56697fde77846a0c146902d3ec57ebe660
macmate <ric@allthingscomputers.com.au>
2024-12-08 17:09:21 +1100

    Add PositionSizing tests with supporting test classes

--- tests/PositionSizingTests.cs
+++ tests/PositionSizingTests.cs
@@ -2,6 +2,8 @@ using System;
 using NUnit.Framework;
 using cAlgo.API;
 using cAlgo.RiskManagement;
+using cAlgo.Tests.Mocks;
+using cAlgo.Tests.TestHelpers;
 
 namespace cAlgo.Tests
 {
@@ -9,49 +11,41 @@ namespace cAlgo.Tests
     public class PositionSizingTests
     {
         private PositionSizing positionSizing;
-        private double riskAmount = 1000; // $1000 risk per trade
         private Symbol mockSymbol;
 
         [SetUp]
         public void Setup()
         {
-            // Create mock symbol with known values
             mockSymbol = new MockSymbol
             {
-                PipValue = 10, // $10 per pip
-                VolumeInUnitsStep = 1000, // Standard lot size step
-                VolumeInUnitsMin = 1000,  // Minimum lot size
-                VolumeInUnitsMax = 100000 // Maximum lot size
+                PipValue = TestConstants.DefaultPipValue,
+                VolumeInUnitsStep = TestConstants.DefaultVolumeStep,
+                VolumeInUnitsMin = TestConstants.MinVolume,
+                VolumeInUnitsMax = TestConstants.MaxVolume
             };
 
-            positionSizing = new PositionSizing(mockSymbol, riskAmount);
+            positionSizing = new PositionSizing(mockSymbol, TestConstants.DefaultRiskAmount);
         }
 
         [Test]
         public void CalculatePositionSize_StandardScenario_CalculatesCorrectly()
         {
-            // Arrange
-            var stopLossInPips = 10; // 10 pips stop loss
-            var expectedSize = 10000; // Expected position size for $1000 risk
+            var stopLossInPips = 10;
+            var expectedSize = 10000;
 
-            // Act
             var result = positionSizing.CalculatePositionSize(stopLossInPips);
 
-            // Assert
             Assert.AreEqual(expectedSize, result);
         }
 
         [Test]
         public void CalculatePositionSize_SmallStopLoss_RoundsToStepSize()
         {
-            // Arrange
-            var stopLossInPips = 5; // 5 pips stop loss
-            var expectedSize = 20000; // Expected position size for $1000 risk
+            var stopLossInPips = 5;
+            var expectedSize = 20000;
 
-            // Act
             var result = positionSizing.CalculatePositionSize(stopLossInPips);
 
-            // Assert
             Assert.AreEqual(expectedSize, result);
             Assert.IsTrue(result % mockSymbol.VolumeInUnitsStep == 0);
         }
@@ -59,22 +53,18 @@ namespace cAlgo.Tests
         [Test]
         public void CalculatePositionSize_LargeStopLoss_RespectsMinimumSize()
         {
-            // Arrange
-            var stopLossInPips = 100; // 100 pips stop loss
+            var stopLossInPips = 100;
             var result = positionSizing.CalculatePositionSize(stopLossInPips);
 
-            // Assert
             Assert.GreaterOrEqual(result, mockSymbol.VolumeInUnitsMin);
         }
 
         [Test]
         public void CalculatePositionSize_SmallStopLoss_RespectsMaximumSize()
         {
-            // Arrange
-            var stopLossInPips = 0.1; // Very small stop loss
+            var stopLossInPips = 0.1;
             var result = positionSizing.CalculatePositionSize(stopLossInPips);
 
-            // Assert
             Assert.LessOrEqual(result, mockSymbol.VolumeInUnitsMax);
         }
 
@@ -88,16 +78,13 @@ namespace cAlgo.Tests
         [Test]
         public void CalculatePositionSize_DifferentRiskAmounts_ScalesCorrectly()
         {
-            // Arrange
-            var standardRisk = new PositionSizing(mockSymbol, 1000);
-            var doubleRisk = new PositionSizing(mockSymbol, 2000);
+            var standardRisk = new PositionSizing(mockSymbol, TestConstants.DefaultRiskAmount);
+            var doubleRisk = new PositionSizing(mockSymbol, TestConstants.DefaultRiskAmount * 2);
             var stopLossInPips = 10;
 
-            // Act
             var standardSize = standardRisk.CalculatePositionSize(stopLossInPips);
             var doubleSize = doubleRisk.CalculatePositionSize(stopLossInPips);
 
-            // Assert
             Assert.AreEqual(standardSize * 2, doubleSize);
         }
 
@@ -106,14 +93,6 @@ namespace cAlgo.Tests
         {
             Assert.Throws<ArgumentException>(() => new PositionSizing(mockSymbol, 0));
             Assert.Throws<ArgumentException>(() => new PositionSizing(mockSymbol, -1000));
-        }
-
-        private class MockSymbol : Symbol
-        {
-            public override double PipValue { get; set; }
-            public override double VolumeInUnitsStep { get; set; }
-            public override double VolumeInUnitsMin { get; set; }
-            public override double VolumeInUnitsMax { get; set; }
         }
     }
 }
---
+++ tests/TestHelpers/TestConstants.cs
@@ -0,0 +1,11 @@
+namespace cAlgo.Tests.TestHelpers
+{
+    public static class TestConstants
+    {
+        public const double DefaultRiskAmount = 1000;
+        public const double DefaultPipValue = 10;
+        public const double DefaultVolumeStep = 1000;
+        public const double MinVolume = 1000;
+        public const double MaxVolume = 100000;
+    }
+}
